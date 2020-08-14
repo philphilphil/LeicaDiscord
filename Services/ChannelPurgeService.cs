c2ff693c4563fde96f5d4ffb92759bfb4cd4bea1
Philipp Baum <philipp.baum@mailbox.org>
2020-08-14 21:27:11 +0200

    working delete

--- Services/ChannelPurgeService.cs
+++ Services/ChannelPurgeService.cs
@@ -3,6 +3,7 @@ using Discord.Commands;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,24 +13,43 @@ namespace KenR_LeicaBot.Services
     {
         public async Task PurgeAsync(SocketCommandContext context)
         {
+            var channel = "bot"; 
+
             var msg = context.Message;
-            var messages = await msg.Channel.GetMessagesAsync(100).FlattenAsync(); //defualt is 100
+            var gallery = context.Guild.TextChannels.Where(x => x.Name == channel).FirstOrDefault();
+            var messages = await gallery.GetMessagesAsync(10000).FlattenAsync(); 
+            int i = 0;
+            int i2 = 0;
 
             // var messages = await msg.Channel.GetMessagesAsync(100);
             foreach (IUserMessage message in messages)
             {
-                if (message.CreatedAt < DateTime.Now.AddDays(-6))
+                //Skip messages with Attachments and special links
+                if (message.Attachments.Count > 0 || ContentInAllowList(message.Content))
                 {
-                    //skip old messages!
+                    i2++;
                     continue;
                 }
 
-                if (message.Content.Contains("gotcha"))
-                {
-                    await message.DeleteAsync(); //rate limit exception if too many messages!!!
-
-                }
+                i++;
+                await message.DeleteAsync(); //rate limit exception if too many messages!!!
             }
+
+            await context.Channel.SendMessageAsync($"Deleted {i} Messages and skipped {i2} in channel {channel}");
+        }
+
+        private bool ContentInAllowList(string content)
+        {
+            List<string> allowedStrings = new List<string>(); //TODO: add this list to config/database
+            allowedStrings.Add("instagram.com");
+            allowedStrings.Add("imgur.com");
+
+            foreach (var item in allowedStrings)
+            {
+                if (content.Contains(item)) return true;
+            }
+
+            return false;
         }
     }
 }
