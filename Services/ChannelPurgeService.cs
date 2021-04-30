134e951ecdc1fac027039b74cf60a5213e3a14dc
Phil Baum <phil@phib.io>
2021-04-30 08:47:23 +0200

    changed delete date of messages to 1 day

--- Services/ChannelPurgeService.cs
+++ Services/ChannelPurgeService.cs
@@ -42,8 +42,8 @@ namespace KenR_LeicaBot.Services
                 //only perform for usermessages
                 if (!(message is IUserMessage)) continue;
 
-                //Skip messages with Attachments and special links. Skip messages younger then 2 days.
-                if (message.Attachments.Count > 0 || ContentInAllowList(message.Content) || message.CreatedAt > DateTime.Now.AddDays(-2))
+                //Skip messages with Attachments and special links. Skip messages younger then 1 day.
+                if (message.Attachments.Count > 0 || ContentInAllowList(message.Content) || message.CreatedAt > DateTime.Now.AddDays(-1))
                 {
                     continue;
                 }
