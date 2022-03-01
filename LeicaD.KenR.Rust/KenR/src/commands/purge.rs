c4f1df8bbc4104834be9fc39d587a17819ea9d11
Phil Baum <philipp.baum@mailbox.org>
2022-03-01 22:50:25 +0100

    purge works

--- LeicaD.KenR.Rust/KenR/src/commands/purge.rs
+++ LeicaD.KenR.Rust/KenR/src/commands/purge.rs
@@ -1,26 +1,45 @@
-use serenity::framework::standard::{macros::command, CommandResult};
-use serenity::model::id::{ChannelId, MessageId};
-use serenity::model::prelude::*;
-use serenity::prelude::*;
+use chrono::Utc;
+use serenity::{
+    framework::standard::{
+        macros::{command},
+        CommandResult,
+    },
+    model::{channel::Message, id::ChannelId},
+    prelude::*, futures::StreamExt,
+};
+use tracing::{error};
 
 #[command]
 async fn purge(ctx: &Context, msg: &Message) -> CommandResult {
-    //msg.channel_id.say(&ctx.http, "Pong!").await?;
-
-    // you can then pass it into a function which retrieves messages:
-    let channel_id = ChannelId(810536807761707121);
-    let _messages = channel_id
-        .messages(&ctx.http, |retriever| {
-            retriever.before(MessageId(945612625356685353)).limit(5)
-        })
-        .await?;
-
-    for m in _messages {
-        if m.attachments.len() == 0 {
-            m.delete(&ctx.http).await.unwrap();
-        }
-
-        println!("{}", m.content);
-    }
+    let purge_count = purge_channel(ChannelId(810536807761707121), ctx).await;
+    let delete_count_msg = format!("Deleted {} messages in channel {}", purge_count, "");
+    msg.channel_id.say(&ctx.http, delete_count_msg).await?;
     Ok(())
+}
+
+async fn purge_channel(channel_id: ChannelId, ctx: &Context) -> u64 {
+    let mut count_deleted = 0;
+    let mut messages = channel_id.messages_iter(&ctx).boxed();
+
+    while let Some(message_result) = messages.next().await {
+        match message_result {
+            Ok(message) => {
+                if message.attachments.len() == 0 && message_older_then_one_day(&message) {
+                    match message.delete(&ctx.http).await {
+                        Ok(_) => count_deleted += 1,
+                        Err(error) => {
+                            error!("Error deleting msg: {}. Error: {}", message.id, error)
+                        }
+                    }
+                }
+            }
+            Err(error) => error!("Error fetching messages: {}", error),
+        }
+    }
+    count_deleted
+}
+
+fn message_older_then_one_day(msg: &Message) -> bool {
+    let now = Utc::now();
+    now.signed_duration_since(msg.timestamp).num_seconds() > 86400
 }
