ed2102d8d6918b31785a90099dc1edac0f02bcf1
Phil Baum <philipp.baum@mailbox.org>
2022-03-02 21:48:12 +0100

    refactor and clean multiple channels

--- gallery_cleaner/src/main.rs
+++ gallery_cleaner/src/main.rs
@@ -6,33 +6,29 @@ use serenity::{
     prelude::*,
 };
 use std::env;
-use tracing::{error};
+use tracing::error;
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().expect("Failed to load .env file");
-    let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let token = env::var("DISCORD_TOKEN").expect("Expected token in env.");
+    let admin_channel = str_to_channel_id(
+        &env::var("ADMIN_CHANNEL_ID").expect("Expected admin channel id in env."),
+    );
+    let channels_to_clean = env::vars().filter(|c| c.0.starts_with("PURGE_CHANNEL_ID"));
 
     let client = Client::builder(&token).await.expect("Err creating client");
+    let ctx = &client.cache_and_http.http;
 
-    // for (_, channelid) in purge_chan_ids {
-    let purge_count = purge_channel("810536807761707121", &client.cache_and_http.http).await;
-
-    //let purge_count = purge_channel(&channelid, ctx).await;
-    let delete_count_msg = format!("Deleted {} messages in channel {}", purge_count, "");
-    // msg.channel_id.say(&ctx.http, delete_count_msg).await?;
-    // }
-
-    ChannelId(818753943642964011)
-        .say(&client.cache_and_http.http, delete_count_msg)
-        .await
-        .unwrap();
-
+    for (_, channelid) in channels_to_clean {
+        let purge_count = purge_channel(&channelid, ctx).await;
+        let delete_count_msg = format!("Deleted {} messages in channel {}", purge_count, channelid);
+        admin_channel.say(ctx, delete_count_msg).await.unwrap();
+    }
 }
 
 async fn purge_channel(channel_id: &str, ctx: &Http) -> u64 {
-    let channel_id: u64 = channel_id.parse().expect("Error parsing purge channel id.");
-    let channel_id = ChannelId(channel_id);
+    let channel_id = str_to_channel_id(channel_id);
     let mut count_deleted = 0;
 
     let mut messages = channel_id.messages_iter(&ctx).boxed();
@@ -57,5 +53,10 @@ async fn purge_channel(channel_id: &str, ctx: &Http) -> u64 {
 
 fn message_older_then_one_day(msg: &Message) -> bool {
     let now = Utc::now();
-    now.signed_duration_since(msg.timestamp).num_seconds() > 1
+    now.signed_duration_since(msg.timestamp).num_seconds() > 86400 // one day in seconds
+}
+
+fn str_to_channel_id(id_as_str: &str) -> ChannelId {
+    let channel_id: u64 = id_as_str.parse().expect("Error parsing purge channel id.");
+    ChannelId(channel_id)
 }
