b6d51160d28ca1672d0b3e18a67807475cfb3a97
Phil Baum <philipp.baum@mailbox.org>
2022-03-06 12:21:58 +0100

    only keep image count

--- gallery_cleaner/src/main.rs
+++ gallery_cleaner/src/main.rs
@@ -41,11 +41,11 @@ async fn main() {
             .unwrap()
             .name; // for some reason .name() on ChannelId does not work.
 
-        let (purge_count, kept_count) = purge_channel(&channel, ctx).await;
+        let (purge_count, count_media_kept) = purge_channel(&channel, ctx).await;
 
         let delete_count_msg = format!(
-            "Deleted {} messages in channel {}. Kept {}.",
-            purge_count, channel_name, kept_count
+            "Deleted {} messages in channel {}. Kept {} images.",
+            purge_count, channel_name, count_media_kept
         );
 
         admin_channel.say(ctx, &delete_count_msg).await.unwrap();
@@ -55,34 +55,36 @@ async fn main() {
 
 async fn purge_channel(channel: &ChannelId, ctx: &Http) -> (u64, u64) {
     let mut count_deleted = 0;
-    let mut count_kept = 0;
+    let mut count_media_kept = 0;
 
     let mut messages = channel.messages_iter(&ctx).boxed();
 
     while let Some(message_result) = messages.next().await {
         match message_result {
             Ok(message) => {
-                if message.attachments.len() == 0
-                    && message_older_then_one_day(&message)
-                    && message_not_cdn(&message)
-                {
+                if message.attachments.len() > 0 || linked_image(&message) {
+                    count_media_kept += 1;
+                } else if message_older_then_one_day(&message) {
                     match message.delete(&ctx).await {
                         Ok(_) => count_deleted += 1,
                         Err(error) => {
                             error!("Error deleting msg: {}. Error: {}", message.id, error)
                         }
                     }
-                } else {
-                    count_kept += 1;
                 }
             }
             Err(error) => error!("Error fetching messages: {}", error),
         }
     }
-    (count_deleted, count_kept)
+
+    (count_deleted, count_media_kept)
 }
 
-fn message_not_cdn(msg: &Message) -> bool {
+fn linked_image(msg: &Message) -> bool {
+    if !msg.content.contains("http") {
+        return false;
+    }
+
     // TODO: move to config
     let allowed_urls = vec![
         "instagram.com",
@@ -93,11 +95,11 @@ fn message_not_cdn(msg: &Message) -> bool {
 
     for url in allowed_urls {
         if msg.content.contains(url) {
-            return false;
+            return true;
         }
     }
 
-    true
+    false
 }
 
 fn message_older_then_one_day(msg: &Message) -> bool {
