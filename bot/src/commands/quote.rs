4fb0e2d879966518f680a6b384168469d45878b7
Phil Baum <philipp.baum@mailbox.org>
2022-05-23 22:51:08 +0200

    refactoring

--- bot/src/commands/quote.rs
+++ bot/src/commands/quote.rs
@@ -30,34 +30,43 @@ fn get_quote() -> Result<String, rusqlite::Error> {
     let path = "db/app.db";
     let conn = Connection::open(path)?;
 
-    let mut quotes = get_random_quote(&conn)?;
+    check_and_reset_quote_states(&conn)?;
 
-    if quotes.len() == 0 {
-        // no quote without LastPosted-Date, resetting
-        conn.execute("UPDATE KenRQuotes SET LastPosted = NULL", [])?;
-        quotes = get_random_quote(&conn)?;
+    if let Ok(quote) = get_random_quote(&conn) {
+        let mut query = conn.prepare("UPDATE KenRQuotes SET LastPosted = DATE() WHERE id = (?)")?;
+        query.execute([quote.id])?;
+        Ok(quote.quote)
     } else {
-        let mut stmt = conn.prepare("UPDATE KenRQuotes SET LastPosted = DATE() WHERE id = (?)")?;
-        stmt.execute([quotes[0].id])?;
+        Err(rusqlite::Error::InvalidQuery)
     }
-
-    Ok(quotes[0].quote.clone())
 }
 
-fn get_random_quote(conn: &Connection) -> Result<Vec<KenRQuote>, rusqlite::Error> {
-    let mut stmt = conn.prepare(
+fn check_and_reset_quote_states(conn: &Connection) -> Result<(), rusqlite::Error> {
+    let available_quote_count: u64 = conn.query_row(
+        "SELECT COUNT(*) FROM KenRQuotes WHERE LastPosted IS NULL",
+        [],
+        |r| r.get(0),
+    )?;
+
+    if available_quote_count == 0 {
+        conn.execute("UPDATE KenRQuotes SET LastPosted = NULL", [])?;
+    }
+
+    Ok(())
+}
+
+fn get_random_quote(conn: &Connection) -> Result<KenRQuote, rusqlite::Error> {
+    let mut query = conn.prepare(
         "SELECT id, quote FROM KenRQuotes WHERE LastPosted IS NULL ORDER BY RANDOM() LIMIT 1;",
     )?;
 
-    let quote = stmt
-        .query_map([], |row| {
-            Ok(KenRQuote {
-                id: row.get(0)?,
-                quote: row.get(1)?,
-            })
-        })?
-        .map(|x| x.unwrap())
-        .collect();
+    let mut quote = query.query_map([], |row| {
+        Ok(KenRQuote {
+            id: row.get(0)?,
+            quote: row.get(1)?,
+        })
+    })?;
 
-    Ok(quote)
+    // its ugly to get just one line but i cant find a better way right now..
+    Ok(quote.next().unwrap().unwrap())
 }
