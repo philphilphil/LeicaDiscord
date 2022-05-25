8d77711c1c34ffd60da1b06d28a831bbf2115efa
Phil Baum <philipp.baum@mailbox.org>
2022-05-25 22:58:19 +0200

    cleanup connection/bot build

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -28,13 +28,37 @@ async fn main() {
     // initialize log output
     tracing_subscriber::fmt::init();
 
-    // Fetch owners and id
-    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    // load owners
+    let http = Http::new(&token);
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        }
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
 
-    // create framework for bot commands
-    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    // initialize framework for commands
+    let framework = StandardFramework::new()
+        .configure(|c| {
+            c.with_whitespace(true)
+                .on_mention(Some(bot_id))
+                .prefix("!")
+                .delimiters(vec![", ", ","])
+                .owners(owners)
+        })
+        .group(&GENERAL_GROUP);
 
-    // build clients, register framework and event handler
+    // set intents and build client
+    let intents = GatewayIntents::all();
     let mut client = Client::builder(token, intents)
         .framework(framework)
         .event_handler(Handler)
@@ -42,7 +66,6 @@ async fn main() {
         .await
         .expect("Error creating client");
 
-    // initialize shared manager container
     {
         let mut data = client.data.write().await;
         data.insert::<ShardManagerContainer>(client.shard_manager.clone());
